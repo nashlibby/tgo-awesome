7bcb3cfbc1aab83b0747a0c357b8185783d9cb9e
nash.tang <112614251@qq.com>
2020-03-29 14:01:45 +0800

    init

--- rabbitmq/client/tutorials/rabbitmq.go
+++ rabbitmq/client/tutorials/rabbitmq.go
@@ -46,41 +46,63 @@ func (r *RabbitMq) Close() {
 	r.conn.Close()
 }
 
-// 交换机定义
+/**
+ ** 交换机定义
+ ** @param durable 消息是否持久保存
+ ** @param autoDelete 当最后一个消费者断开连接之后交换机是否自动被删除
+ ** @param internal 设置是否内置，true为内置。如果是内置交换器，客户端无法发送消息到这个交换器中，只能通过交换器路由到交换器这种方式
+ ** @param noWait 是否非阻塞，true表示是。阻塞：表示创建交换器的请求发送后，阻塞等待Server返回信息。非阻塞：不会阻塞等待Server的返回信息
+ */
 func (r *RabbitMq) ExchangeDeclare(exchangeName, exchangeType string, durable, autoDelete, internal, noWait bool) {
-	err := r.ch.ExchangeDeclare(
-		exchangeName,   // name
-		exchangeType, // type
-		durable,     // durable
-		autoDelete,    // auto-deleted
-		internal,    // internal
-		noWait,    // no-wait
-		nil,      // arguments
-	)
-
+	err := r.ch.ExchangeDeclarePassive(exchangeName, exchangeType, durable, autoDelete, internal, noWait, nil)
 	if err != nil {
-		panic(err.Error())
+		err = r.ch.ExchangeDeclare(
+			exchangeName,   // name
+			exchangeType, // type
+			durable,     // durable
+			autoDelete,    // auto-deleted
+			internal,    // internal
+			noWait,    // no-wait
+			nil,      // arguments
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 }
 
-// 队列定义
+/**
+ ** 队列定义
+ ** @param durable 消息是否持久保存
+ ** @param autoDelete 当最后一个消费者断开连接之后队列是否自动被删除
+ ** @param exclusive 是否排外 1.当连接关闭时该队列是否会自动删除 2.如果不是排外的，可以使用两个消费者都访问同一个队列，如果是排外的，会对当前队列加锁，其他通道channel是不能访问的
+ ** @param noWait 是否非阻塞，true表示是。阻塞：表示创建交换器的请求发送后，阻塞等待Server返回信息。非阻塞：不会阻塞等待Server的返回信息
+ */
 func (r *RabbitMq) QueueDeclare(queueName string, durable, autoDelete, exclusive, noWait bool) *amqp.Queue{
-	queue, err := r.ch.QueueDeclare(
-		queueName,
-		durable,
-		autoDelete,
-		exclusive,
-		noWait,
-		nil,
-	)
+	queue, err := r.ch.QueueDeclarePassive(queueName, durable, autoDelete, exclusive, noWait, nil)
 	if err != nil {
-		panic(err.Error())
+		queue, err = r.ch.QueueDeclare(
+			queueName,
+			durable,
+			autoDelete,
+			exclusive,
+			noWait,
+			nil,
+		)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 
 	return &queue
 }
 
-// 投递消息
+/**
+ ** 投递消息
+ ** @param mandatory 如果没有匹配到符合条件的队列 false 丢弃消息 true 会调用basic.return方法将消息返回给生产者
+ ** @param immediate 如果队列没有消费者 false 丢弃消息 true 会调用basic.return方法将消息返回给生产者
+ */
 func (r *RabbitMq) Publish(body []byte, exchangeName, routingKey string, mandatory, immediate bool) error {
 	err := r.ch.Publish(
 		exchangeName,
--- rabbitmq/server/tutorials/rabbitmq.go
+++ rabbitmq/server/tutorials/rabbitmq.go
@@ -47,41 +47,62 @@ func (r *RabbitMq) Close() {
 	r.conn.Close()
 }
 
-// 交换机定义
+/**
+ ** 交换机定义
+ ** @param durable 消息是否持久保存
+ ** @param autoDelete 当最后一个消费者断开连接之后交换机是否自动被删除
+ ** @param internal 设置是否内置，true为内置。如果是内置交换器，客户端无法发送消息到这个交换器中，只能通过交换器路由到交换器这种方式
+ ** @param noWait 是否非阻塞，true表示是。阻塞：表示创建交换器的请求发送后，阻塞等待Server返回信息。非阻塞：不会阻塞等待Server的返回信息
+ */
 func (r *RabbitMq) ExchangeDeclare(exchangeName, exchangeType string, durable, autoDelete, internal, noWait bool) {
-	err := r.ch.ExchangeDeclare(
-		exchangeName,   // name
-		exchangeType, // type
-		durable,     // durable
-		autoDelete,    // auto-deleted
-		internal,    // internal
-		noWait,    // no-wait
-		nil,      // arguments
-	)
-
+	err := r.ch.ExchangeDeclarePassive(exchangeName, exchangeType, durable, autoDelete, internal, noWait, nil)
 	if err != nil {
-		panic(err.Error())
+		err = r.ch.ExchangeDeclare(
+			exchangeName,   // name
+			exchangeType, // type
+			durable,     // durable
+			autoDelete,    // auto-deleted
+			internal,    // internal
+			noWait,    // no-wait
+			nil,      // arguments
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 }
 
-// 队列定义
+/**
+ ** 队列定义
+ ** @param durable 消息是否持久保存
+ ** @param autoDelete 当最后一个消费者断开连接之后队列是否自动被删除
+ ** @param exclusive 是否排外 1.当连接关闭时该队列是否会自动删除 2.如果不是排外的，可以使用两个消费者都访问同一个队列，如果是排外的，会对当前队列加锁，其他通道channel是不能访问的
+ ** @param noWait 是否非阻塞，true表示是。阻塞：表示创建交换器的请求发送后，阻塞等待Server返回信息。非阻塞：不会阻塞等待Server的返回信息
+ */
 func (r *RabbitMq) QueueDeclare(queueName string, durable, autoDelete, exclusive, noWait bool) *amqp.Queue{
-	queue, err := r.ch.QueueDeclare(
-		queueName,
-		durable,
-		autoDelete,
-		exclusive,
-		noWait,
-		nil,
-	)
+	queue, err := r.ch.QueueDeclarePassive(queueName, durable, autoDelete, exclusive, noWait, nil)
 	if err != nil {
-		panic(err.Error())
+		queue, err = r.ch.QueueDeclare(
+			queueName,
+			durable,
+			autoDelete,
+			exclusive,
+			noWait,
+			nil,
+		)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 
 	return &queue
 }
 
-// 队列绑定
+/**
+ ** 队列绑定
+ ** @param noWait 是否非阻塞，true表示是。阻塞：表示创建交换器的请求发送后，阻塞等待Server返回信息。非阻塞：不会阻塞等待Server的返回信息
+ */
 func (r *RabbitMq) QueueBind(queueName, routingKey, exchangeName string, noWait bool) {
 	err := r.ch.QueueBind(
 		queueName, // queue name
@@ -94,7 +115,13 @@ func (r *RabbitMq) QueueBind(queueName, routingKey, exchangeName string, noWait 
 	}
 }
 
-// 消费
+/**
+ ** 消费
+ ** @param autoAck 是否自动回复ACK，true为是，回复ACK表示告诉服务器我收到消息了
+ ** @param exclusive 是否排外 1.当连接关闭时该队列是否会自动删除 2.如果不是排外的，可以使用两个消费者都访问同一个队列，如果是排外的，会对当前队列加锁，其他通道channel是不能访问的
+ ** @param noLocal 如果为true，表示生产者和消费者不能是同一个connect
+ ** @param noWait 是否非阻塞，true表示是。阻塞：表示创建交换器的请求发送后，阻塞等待Server返回信息。非阻塞：不会阻塞等待Server的返回信息
+ */
 func (r *RabbitMq) Consume(queueName, consumerName string, autoAck, exclusive, noLocal, noWait bool) {
 	msgList, err := r.ch.Consume(
 		queueName, // queue
@@ -113,6 +140,11 @@ func (r *RabbitMq) Consume(queueName, consumerName string, autoAck, exclusive, n
 
 	go func() {
 		for msg := range msgList {
+			// 如果autoAck为false则手动ack
+			// 注意： 这里只要调用了ack就是手动确认模式，
+			// multiple 表示的是在此channel中先前所有未确认的msg都将被确认
+			// 并不是表示设置为false就不进行当前ack确认
+			// msg.Ack(true)
 			log.Printf("[x] Received a message: %s", msg.Body)
 		}
 	}()
